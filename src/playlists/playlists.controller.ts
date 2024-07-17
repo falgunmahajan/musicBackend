1262bb17b1bd87e1d7d4d5bf0f74fe4bbbd99f5b
Falgun-Mahajan-Avisoft <falgun.mahajan@avisoft.io>
2024-07-17 17:59:41 +0530

    Implemented songs, playlist and auth services

--- src/playlists/playlists.controller.ts
+++ src/playlists/playlists.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { PlaylistsService } from './playlists.service';
 import { CreatePlayListDTO } from './dto/createPlaylist.dto';
-import { Playlist } from './playlist.entity';
 
 @Controller('playlists')
 export class PlaylistsController {
